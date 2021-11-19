4f66d3b8f57d0a40da8ef6d51c7fa8c17d4800e2
erdnando <erdnando@gmail.com>
2021-11-19 12:49:00 -0600

    ok

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import io from 'socket.io-client';
 
 Vue.config.productionTip = false;
 
-const socket = io('http://192.168.0.4');
+//const socket = io('http://192.168.0.4');
+const socket = io(process.env.websocket_host);
+
 
 Vue.use(VueSocketIOExt, socket, { store });
 
