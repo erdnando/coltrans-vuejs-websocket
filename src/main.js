424ea628d1dad2ed328b7b56ae529fedef3c25cf
erdnando <erdnando@gmail.com>
2021-11-22 11:02:18 -0600

    ok

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import io from 'socket.io-client';
 
 Vue.config.productionTip = false;
 
-const socket = io('http://coltrans');
+const socket = io('http://192.168.0.4');
 //const socket = io('http://localhost:9001');
 
 Vue.use(VueSocketIOExt, socket, { store });
